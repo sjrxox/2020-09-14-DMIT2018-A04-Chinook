40c1d65d080afa897fe04e38bb0e2fbd8803d836
sjrxox <46492908+sjrxox@users.noreply.github.com>
2020-09-17 09:11:22 -0600

    Created Context for Artist

--- ChinookSystem/DAL/ChinookSystemContext.cs
+++ ChinookSystem/DAL/ChinookSystemContext.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Additional Namespaces
+using System.Data.Entity;
+using ChinookSystem.ENTITIES;
+
 namespace ChinookSystem.DAL
 {
-    class ChinookSystemContext
+    internal class ChinookSystemContext : DbContext
     {
+        public ChinookSystemContext() : base("name=ChinookDB")
+        {
+
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        //public DbSet<Album> Albums { get; set; }
     }
 }
