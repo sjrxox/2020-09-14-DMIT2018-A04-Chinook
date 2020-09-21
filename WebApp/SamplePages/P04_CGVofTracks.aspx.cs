c8d526599222b345796bca43b32d428565c9b97c
sjrxox <46492908+sjrxox@users.noreply.github.com>
2020-09-21 15:59:13 -0600

    Minor changes to CGVofTracks. Completed and working CGVofTracks and when a particular is selected to present in a MessageLabel the TrackName

--- WebApp/SamplePages/P04_CGVofTracks.aspx.cs
+++ WebApp/SamplePages/P04_CGVofTracks.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,14 @@ namespace WebApp.SamplePages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow agvrow = GridView1.Rows[GridView1.SelectedIndex];
+            string colValue = agvrow.Cells[2].Text.Trim();
+            MessageLabel.Text = colValue;
 
         }
     }
--- WebApp/SamplePages/P04_CGVofTracks.aspx.designer.cs
+++ WebApp/SamplePages/P04_CGVofTracks.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace WebApp.SamplePages
     {
 
         /// <summary>
-        /// GridView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
-
-        /// <summary>
         /// MessageLabel control.
         /// </summary>
         /// <remarks>
@@ -40,5 +31,14 @@ namespace WebApp.SamplePages
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSource1;
+
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
     }
 }
