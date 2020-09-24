1454ccfc13d7dd61fbf90f6a23455604c67a4865
sjrxox <46492908+sjrxox@users.noreply.github.com>
2020-09-24 08:30:54 -0600

    Created AlbumsByArtist method in AlbumController

--- ChinookSystem/BLL/AlbumController.cs
+++ ChinookSystem/BLL/AlbumController.cs
@@ -11,6 +11,7 @@ using ChinookSystem.VIEWMODELS;
 
 namespace ChinookSystem.BLL
 {
+    #region Queries
     [DataObject]
     public class AlbumController
     {
@@ -31,5 +32,25 @@ namespace ChinookSystem.BLL
                 return results.ToList();
             }
         }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<AlbumViewModel> AlbumsByArtist(int artistid)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                var results = from x in context.Albums
+                              where x.ArtistId == artistid
+                              select new AlbumViewModel
+                              {
+                                  AlbumId = x.AlbumId,
+                                  AlbumTitle = x.Title,
+                                  ArtistId = x.ArtistId,
+                                  AlbumReleaseYear = x.ReleaseYear,
+                                  AlbumReleaseLabel = x.ReleaseLabel
+                              };
+                return results.ToList();
+            }
+        }
     }
+    #endregion
 }
