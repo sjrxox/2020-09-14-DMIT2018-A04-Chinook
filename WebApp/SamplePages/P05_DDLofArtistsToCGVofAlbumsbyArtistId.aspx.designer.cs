f43610e3037bc645421c99775688e865d13bbef7
sjrxox <46492908+sjrxox@users.noreply.github.com>
2020-09-24 09:03:57 -0600

    FrontEnd Working

--- WebApp/SamplePages/P05_DDLofArtistsToCGVofAlbumsbyArtistId.aspx.designer.cs
+++ WebApp/SamplePages/P05_DDLofArtistsToCGVofAlbumsbyArtistId.aspx.designer.cs
@@ -51,6 +51,15 @@ namespace WebApp.SamplePages
         protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
+        /// ObjectDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSource2;
+
+        /// <summary>
         /// GridView1 control.
         /// </summary>
         /// <remarks>
