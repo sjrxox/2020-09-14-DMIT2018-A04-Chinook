201a78f83cf5e102d03c9d2314538d95750a441c
sjrxox <46492908+sjrxox@users.noreply.github.com>
2020-10-22 09:19:48 -0600

    Copy from robbin's repo

---
+++ ChinookSystem/BLL/PlayListController.cs
@@ -0,0 +1,42 @@
+﻿using ChinookSystem.DAL;
+using ChinookSystem.ENTITIES;
+using ChinookSystem.VIEWMODELS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class PlayListController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<PlayListItem> PlayList_GetPlayListOfSSize(int lowestplaylistsize)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+				var results = from x in context.Playlists
+							  where x.PlaylistTracks.Count() >= lowestplaylistsize
+							  orderby x.UserName
+							  select new PlayListItem
+							  {
+								  Name = x.Name,
+								  TrackCount = x.PlaylistTracks.Count(),
+								  UserName = x.UserName,
+								  Songs = from y in x.PlaylistTracks
+										  orderby y.Track.Genre.Name, y.Track.Name
+										  select new PlayListSong
+										  {
+											  Song = y.Track.Name,
+											  GenreName = y.Track.Genre.Name
+										  }
+							  };
+				return results.ToList();
+			}
+        }
+    }
+}
---
+++ WebApp/SamplePages/ODSRepeater.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ODSRepeater.aspx.cs" Inherits="WebApp.SamplePages.ODSRepeater" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <%--Copy all from robbin's repo--%>
+    
+</asp:Content>
