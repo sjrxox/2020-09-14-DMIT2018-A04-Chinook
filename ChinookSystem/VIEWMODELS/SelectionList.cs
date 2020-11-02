04edc7444b711362e69f7d8ef76a3dd5e3281fb2
sjrxox <46492908+sjrxox@users.noreply.github.com>
2020-11-02 16:33:33 -0700

    Copy missing items from robbin's repo

---
+++ ChinookSystem/VIEWMODELS/SelectionList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.VIEWMODELS
+{
+    public class SelectionList
+    {
+        //copy from robbin repo
+    }
+}
---
+++ ChinookSystem/VIEWMODELS/TrackList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.VIEWMODELS
+{
+    public class TrackList
+    {
+        // cooy from robbin's repo
+    }
+}
